24b5dcf978a743f9b24274ecda5b306c1437930d
priyadharshini312 <142963980+priyadharshini312@users.noreply.github.com>
2026-03-23 19:30:06 +0530

    Time: 1 ms (99.33%) | Memory: 45.2 MB (98.52%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+                if(count==1){
+                    return nums[i];
+                }
+            }
+        }
+        return -1;
+       }
+    }
