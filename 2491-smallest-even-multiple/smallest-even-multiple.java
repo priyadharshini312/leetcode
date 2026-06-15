998aa54bc9931d83d575f902cd8b9f5720384ffe
priyadharshini312 <142963980+priyadharshini312@users.noreply.github.com>
2026-06-15 10:58:43 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (83.71%) - LeetSync

--- 2491-smallest-even-multiple/smallest-even-multiple.java
+++ 2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,8 +1,15 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if(n%2==0){
+        /*if(n%2==0){
             return n;
         }
         return n*2;
+        if(n%2!=0){
+            return n*2;
+        }
+        else{
+        return n;
+        }*/
+        return n*(n%2+1);
     }
 }
