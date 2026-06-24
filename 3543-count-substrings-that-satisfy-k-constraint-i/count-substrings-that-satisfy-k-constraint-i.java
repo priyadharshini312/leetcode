d6898d9e662f8021400e657abdc2ead0f6228443
priyadharshini312 <142963980+priyadharshini312@users.noreply.github.com>
2026-06-24 14:59:01 +0530

    Time: 3 ms (36.00%) | Memory: 43.2 MB (94.75%) - LeetSync

--- 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.java
+++ 3543-count-substrings-that-satisfy-k-constraint-i/count-substrings-that-satisfy-k-constraint-i.java
@@ -3,18 +3,15 @@ class Solution {
         String m="";
         int count=0;
         for(int i=0;i<s.length();i++){
+            int one=0,zero=0;
             for(int j=i;j<s.length();j++){
-                m=s.substring(i,j+1);
-                int one=0,zero=0;
-                for(int l=0;l<m.length();l++){
-                    
-                    if(m.charAt(l)=='1'){
+                
+                if(s.charAt(j)=='1'){
                         one++;
                     }
                     else{
                         zero++;
                     }
-                }
                     if(one<=k ||zero<=k){
                          count++;
                     }
