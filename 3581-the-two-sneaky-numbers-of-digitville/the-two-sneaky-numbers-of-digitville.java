1037045b66ca1becbee9d981adf1a9c4d6363914
priyadharshini312 <142963980+priyadharshini312@users.noreply.github.com>
2026-06-25 14:22:27 +0530

    Time: 5 ms (15.01%) | Memory: 46.4 MB (44.97%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        /*HashMap<Integer,Integer> map=new HashMap<>();
         for(int i:nums){
             map.put(i,map.getOrDefault(i,0)+1);
         }
@@ -11,6 +11,16 @@ class Solution {
                 arr[j]=val;
                 j++;
             }
+        }
+        return arr;*/
+        Arrays.sort(nums);
+        int arr[]=new int[2];
+        int j=0;
+        for(int i=0;i<nums.length-1;i++){
+             if(nums[i]==nums[i+1]){
+                arr[j]=nums[i];
+                j++;
+             }
         }
         return arr;
     }
