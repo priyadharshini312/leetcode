f010a01ac89204827eed8b2185e2226fbdf3c2ac
priyadharshini312 <142963980+priyadharshini312@users.noreply.github.com>
2026-07-01 16:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (82.74%) - LeetSync

--- 3632-button-with-longest-push-time/button-with-longest-push-time.java
+++ 3632-button-with-longest-push-time/button-with-longest-push-time.java
@@ -2,20 +2,20 @@ class Solution {
     public int buttonWithLongestTime(int[][] events) {
         int index=0;
         int max=events[0][1];
-        int val=events[0][0];
-        int diff=0;
+        int ans=events[0][0];
         for(int i=1;i<events.length;i++){
-              diff=events[i][1]-events[i-1][1];
+              int diff=events[i][1]-events[i-1][1];
+              int val=events[i][0];
               if(max<diff){
                 max=diff;
-                val=events[i][0];
+                ans=val;
               }
-              if(max==diff){
+              if(max==diff && val<ans){
                 max=diff;
-                val=Math.min(val,events[i][0]);
+                ans=val;
               }
 
         }
-        return val;
+        return ans;
     }
 }
