47ed138ff3224ed6d398d005cc253cb986435293
priyadharshini312 <142963980+priyadharshini312@users.noreply.github.com>
2026-07-23 09:38:14 +0530

    Time: 9 ms (5.36%) | Memory: 44.7 MB (86.01%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,22 +1,34 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int arr[]=new int[nums1.length];
-        for(int i=0;i<nums2.length;i++){
-            map.put(nums2[i],i);
+    public int[] nextGreaterElement(int[] arr1, int[] arr2) {
+        List<Integer>list=new ArrayList();
+		for(int i=0;i<arr1.length;i++) {
+			for(int j=0;j<arr2.length;j++) {
+				if(arr1[i]==arr2[j]){
+					if(j==arr2.length-1) {
+						list.add(-1);
+					}
+					else {
+						int s=list.size();
+						for(int k=j+1;k<arr2.length;k++) {
+							if(arr2[k]>arr1[i]) {
+								list.add(arr2[k]);
+								break;
+							}
+						}
+						if(list.size()==s) {
+							list.add(-1);
+						}
+						
+					}
+					
+				}
+			}
+			
+		}
+        int nums[]=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            nums[i]=list.get(i);
         }
-        for(int i=0;i<nums1.length;i++){
-            int ins=-1;
-               if(map.containsKey(nums1[i])){
-                   for(int j=map.get(nums1[i]);j<nums2.length-1;j++){
-                      if(nums2[j+1]>nums1[i]){
-                          ins=nums2[j+1];
-                          break;
-                      }
-                   }
-               }
-                 arr[i]=ins;
-                }
-        return arr;
+        return nums;
     }
 }
