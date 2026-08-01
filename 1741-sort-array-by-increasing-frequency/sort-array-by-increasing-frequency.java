35e0010f16e8977f640cf6564af4bd0af603c921
priyadharshini312 <142963980+priyadharshini312@users.noreply.github.com>
2026-08-01 07:43:54 +0530

    Time: 10 ms (21.10%) | Memory: 45.8 MB (34.93%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        int j=0;
+        LinkedHashMap<Integer,Integer> map=new LinkedHashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        
+        while(!map.isEmpty()){
+            int min=Integer.MAX_VALUE;
+            int val=0;
+          for(int key:map.keySet()){
+
+               if(map.get(key)<min){
+                 min=map.get(key);
+                 val=key;
+               }
+               else if(map.get(key)==min){
+                val=Math.max(val,key);
+                min=map.get(key);
+    
+               }
+              
+        }
+        int i=0;
+        while(i<min){
+            nums[j++]=val;
+            i++;
+        }
+        map.remove(val);
+        }
+        return nums;
+        
+    }
+}
